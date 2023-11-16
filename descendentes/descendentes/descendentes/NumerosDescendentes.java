071982fea3599a99a1c3ab68af64cf6725542763
victo <victo@MSI.home>
2023-11-16 11:49:31 +0100

    del 1 al 10

---
+++ descendentes/descendentes/descendentes/NumerosDescendentes.java
@@ -0,0 +1,12 @@
+package descendentes;
+
+		public class NumerosDescendentes {
+		    public static void main(String[] args) {
+		        int numero = 10; // Iniciamos desde el 10
+
+		        while (numero >= 1) {
+		            System.out.println(numero);
+		            numero--; // Decrementamos el número en cada iteración
+		        }
+}
+		}
