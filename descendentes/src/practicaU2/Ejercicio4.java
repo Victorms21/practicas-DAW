75ab714b9a3a1502c533bda06e0133cd50bb77bc
victo <victo@MSI.home>
2023-11-21 14:03:24 +0100

    eje4

--- descendentes/src/practicaU2/Ejercicio4.java
+++ descendentes/src/practicaU2/Ejercicio4.java
@@ -25,47 +25,54 @@ public class Ejercicio4 {
 		System.out.println("2. Descendente");
 		int opcion = scanner.nextInt();
 		
-		//Se crea un array que almacene los datos de los números ingresados.
-		int[]numeros = {num1, num2, num3};
-		
 		//Ordenar los números
-		 if (opcion == 1) {
-	            for (int i = 0; i < numeros.length - 1; i++) {
-	                for (int j = 0; j < numeros.length - i - 1; j++) {
-	                    if (numeros[j] > numeros[j + 1]) {
-	                        // Intercambiar los números si están en el orden incorrecto
-	                        int temp = numeros[j];
-	                        numeros[j] = numeros[j + 1];
-	                        numeros[j + 1] = temp;
-	                    }
-	                }
-	            }
-	            System.out.println("Números en orden ascendente:");
-	            for (int num : numeros) {
-	                System.out.print(num + " ");
-	            }
-	        } else if (opcion == 2) {
-	            for (int i = 0; i < numeros.length - 1; i++) {
-	                for (int j = 0; j < numeros.length - i - 1; j++) {
-	                    if (numeros[j] < numeros[j + 1]) {
-	                        // Intercambiar los números si están en el orden incorrecto
-	                        int temp = numeros[j];
-	                        numeros[j] = numeros[j + 1];
-	                        numeros[j + 1] = temp;
-	                    }
-	                }
-	            }
-	            System.out.println("Números en orden descendente:");
-	            for (int num : numeros) {
-	                System.out.print(num + " ");
-	            }
-	        } else {
-	            System.out.println("Opción no válida. Debes seleccionar 1 o 2.");
-	        }
+		switch (opcion) {
+	     case 1:
+             if (num1 > num2) {
+                 int temp = num1;
+                 num1 = num2;
+                 num2 = temp;
+             }
+             if (num2 > num3) {
+                 int temp = num2;
+                 num2 = num3;
+                 num3 = temp;
+             }
+             if (num1 > num2) {
+                 int temp = num1;
+                 num1 = num2;
+                 num2 = temp;
+             }
+             System.out.println("Números en orden ascendente: " + num1 + ", " + num2 + ", " + num3);
+             break;
+         case 2:
+             if (num1 < num2) {
+                 int temp = num1;
+                 num1 = num2;
+                 num2 = temp;
+             }
+             if (num2 < num3) {
+                 int temp = num2;
+                 num2 = num3;
+                 num3 = temp;
+             }
+             if (num1 < num2) {
+                 int temp = num1;
+                 num1 = num2;
+                 num2 = temp;
+             }
+             System.out.println("Números en orden descendente: " + num1 + ", " + num2 + ", " + num3);
+             break;
+         default:
+             System.out.println("Opción no válida. Debes seleccionar 1 o 2.");
+     }
 
-	        scanner.close();
-	    }
-		
+     scanner.close();
+ }
 }
+
+		
+	
+
 
 
